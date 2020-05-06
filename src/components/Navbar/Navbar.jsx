8cc33fd6e0b4d2525bf8acbe65f9c0dacc255224
Vpartyzan <peregrim.mt@gmail.com>
2020-05-06 22:41:51 +0300

    replace a-link to NavLink in Navbar; set color to active link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import classes from './Navbar.module.css';
+import s from './Navbar.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className={classes.nav}>
-        <div className={classes.item}>
-          <a href="/profile">Profile</a>
+        <nav className={s.nav}>
+        <div className={s.item}>
+          <NavLink to="/profile" activeClassName={s.activeLink}>Profile</NavLink>
         </div>
-        <div className={classes.item}>
-          <a href="/dialogs">Messages</a>
+        <div className={s.item}>
+          <NavLink to="/dialogs" activeClassName={s.activeLink}>Messages</NavLink>
         </div>
-        <div className={classes.item}>
-          <a href='/news'>News</a>
+        <div className={s.item}>
+          <NavLink to='/news' activeClassName={s.activeLink}>News</NavLink>
         </div>
-        <div className={classes.item}>
-          <a href='/music'>Music</a>
+        <div className={s.item}>
+          <NavLink to='/music' activeClassName={s.activeLink}>Music</NavLink>
         </div>
-        <div className={classes.item}>
-          <a href='/setting'>Setting</a>
+        <div className={s.item}>
+          <NavLink to='/setting' activeClassName={s.activeLink}>Setting</NavLink>
         </div>
       </nav>
     );
