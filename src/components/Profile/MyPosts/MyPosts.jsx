8a535f938301afd07fdf41e0f3aff45ea59bb9ad
Vpartyzan <peregrim.mt@gmail.com>
2020-05-20 23:15:47 +0300

    action creator; action type

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
+import { updateNewPostTextActionCreator, addPostActionCreator } from '../../../Redux/state';
 
 const MyPosts = (props) => {
 
@@ -9,13 +10,14 @@ const MyPosts = (props) => {
     let newPostElement = React.createRef();
 
     let addPost = () => {
-        props.dispatch({type: 'ADD-POST'});
+        props.dispatch(addPostActionCreator());
     };
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        props.dispatch({type: 'UPDATE-NEW-POST-TEXT', newText: text});
-    }
+        let action = updateNewPostTextActionCreator(text);
+        props.dispatch(action);
+    };
 
     return (
         <div className={s.postBlock}>
