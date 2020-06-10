8771c68060a57a41a61410a3ce7e9c26a385d307
Vpartyzan <peregrim.mt@gmail.com>
2020-06-10 23:20:47 +0300

    install axios; add get request to server; refactoring User to Class-component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,51 +1,50 @@
 import React from 'react';
-import styles from './users.module.css'
+import styles from './users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/userPhoto.png';
 
-let Users = (props) => {
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
 
-    if (props.users.length === 0) {
-        props.setUsers( [
-            { id: 1, photoUrl: 'https://www.ferra.ru/thumb/1800x0/filters:quality(75):no_upscale()/imgs/2019/10/17/10/3613124/f133cf3f7049a7f22f34a194c990d6b52d745657.png',
-                followed: false, fullName: 'Neo', status: 'the One', location: {city: 'NY', country: 'USA'}},
-            { id: 2, photoUrl: 'https://www.ferra.ru/thumb/1800x0/filters:quality(75):no_upscale()/imgs/2019/10/17/10/3613124/f133cf3f7049a7f22f34a194c990d6b52d745657.png',
-                followed: true, fullName: 'Max', status: 'Learn JS', location: {city: 'Minsk', country: 'Belarus'}},
-            { id: 3, photoUrl: 'https://www.ferra.ru/thumb/1800x0/filters:quality(75):no_upscale()/imgs/2019/10/17/10/3613124/f133cf3f7049a7f22f34a194c990d6b52d745657.png',
-                followed: true, fullName: 'Taras', status: 'in Matrix', location: {city: 'Kiev', country: 'Ukraine'}},
-            { id: 4, photoUrl: 'https://www.ferra.ru/thumb/1800x0/filters:quality(75):no_upscale()/imgs/2019/10/17/10/3613124/f133cf3f7049a7f22f34a194c990d6b52d745657.png',
-                followed: false, fullName: 'Grzegorz', status: 'in Matrix', location: {city: 'Krakow', country: 'Poland'}}
-        ]);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(respons => {
+                this.props.setUsers(respons.data.items);
+            });
     };
 
-    return (
-        <div>
-            {
-                props.users.map(u => <div key={u.id}>
+    render() {
+        return (
+            <div>
+                {
+                    this.props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img src={u.photoUrl} className={styles.userPhoto}/>
+                            <img src={ u.photos.small !== null ? u.photos.small : userPhoto } className={styles.userPhoto}/>
                         </div>
                         <div>
                             { u.followed
-                                ? <button onClick={ () => { props.unfollow(u.id)} }>Unfollow</button>
-                                : <button onClick={ () => { props.follow(u.id)} }>Follow</button>
+                                ? <button onClick={ () => { this.props.unfollow(u.id)} }>Unfollow</button>
+                                : <button onClick={ () => { this.props.follow(u.id)} }>Follow</button>
                             }
 
                         </div>
                     </span>
-                    <span>
                         <span>
-                            <div>{u.fullName}</div>
+                        <span>
+                            <div>{u.name}</div>
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>{"u.location.country"}</div>
+                            <div>{"u.location.city"}</div>
                         </span>
                     </span>
-                </div>)
-            }
-        </div>
-    );
-};
+                    </div>)
+                }
+            </div>
+        );
+    }
+}
 
 export default Users;
